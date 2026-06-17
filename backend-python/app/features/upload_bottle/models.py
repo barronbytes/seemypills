bc6e46ff51c575f16692a646fb53bb08888e0974
barronbytes <jonathan.m.barron@gmail.com>
2026-06-16 22:16:50 -0400

    docs(backend-python): updated models docstring for Bottle API

--- backend-python/app/features/upload_bottle/models.py
+++ backend-python/app/features/upload_bottle/models.py
@@ -8,7 +8,6 @@ from app.db.mixins import PrimaryKeyMixin, TimestampMixin, SoftDeleteMixin
 class Bottle(Base, PrimaryKeyMixin, TimestampMixin, SoftDeleteMixin):
     """
     Defines table for storing extracted medication bottle information.
-    Requires a valid relation to an originating tracking device.
     Both brand_name and ocr_raw_text are strictly required data points.
     """
     __tablename__ = "pill_bottle"
